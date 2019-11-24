be2ffb99f233eb63a62d1b4222d13c43f6553cd5
Yanzhi Wu <YANZHI@Yanzhis-MacBook-Air.local>
2019-11-24 16:36:17 -0600

    individual project

---
+++ ProjectFolder/Assets/Scripts/PlayAudiosource.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudiosource : MonoBehaviour
+{
+    AudioSource sound;
+    [SerializeField] float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var leftDrumstickPos = GameObject.Find("LeftDrumstick").transform.position;
+        var rightDrumstickPos = GameObject.Find("RightDrumstick").transform.position;
+        if ((Mathf.Abs(leftDrumstickPos.x - transform.position.x) < range)
+            && (Mathf.Abs(leftDrumstickPos.y - transform.position.y) < range)
+            && (Mathf.Abs(leftDrumstickPos.z - transform.position.z) < range))
+        {
+            sound.Play();
+        }
+        if ((Mathf.Abs(rightDrumstickPos.x - transform.position.x) < range)
+            && (Mathf.Abs(rightDrumstickPos.y - transform.position.y) < range)
+            && (Mathf.Abs(rightDrumstickPos.z - transform.position.z) < range))
+        {
+            sound.Play();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# individual-project-RileyWu
+### *A VR Drum Set Simulator that allows players to practice playing a drum set*
+#### To play the game, simply grab the drumsticks and hit any of the drums or cymbals.
+
+------
+
+#### Link to the Windows Build Folder:
+#### https://drive.google.com/drive/folders/1AfW2_i_nZ0SS28CGCLqIn9-9dvL77hSw?usp=sharing
+
+------
